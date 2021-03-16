a3bb768e603cc768c76a7809e90282802fc62f6e
chungontsui <chungont@gmail.com>
2021-03-16 23:55:17 +1300

    Created DAO; Created Test project

--- Recruit.Repository/CustomerDetailDAO.cs
+++ Recruit.Repository/CustomerDetailDAO.cs
@@ -1,17 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Recruit.Data.Model;
 
 namespace Recruit.Repository
 {
 	public class CustomerDetailDAO
 	{
-		public CustomerDetailDAO()
+		private CustomerDetailsContext _context;
+		public CustomerDetailDAO(CustomerDetailsContext context)
 		{
-
+			this._context = context;
 		}
-		
-		//public GetAllCustomerDetails()
-		//{
 
-		//}
+		public IEnumerable<CustomerDetail> GetAllCustomerDetails()
+		{
+			return _context.customerDetails;
+		}
+
+		public void AddCustomerDetail(CustomerDetail customerDetail)
+		{
+			_context.customerDetails.Add(customerDetail);
+		}
+
+		public CustomerDetail GetCustomerDetailById(int id)
+		{
+			return _context.customerDetails.Single<CustomerDetail>(c => c.Id.Equals(id));
+		}
 	}
 }
---
+++ Recruit.Test/DAOTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Recruit.Test
+{
+	
+	public class DAOTest
+	{
+		[Test]
+		public void FirstTest()
+		{
+			Assert.That(1 == 1, "This is Right");
+		}
+	}
+}
